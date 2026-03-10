cd9aee51480a678299cdc36e5da68477fa46096e
Dara G <dara@dara>
2026-03-10 11:35:39 +0200

    retake

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Category, Author, Tag
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Tag)
